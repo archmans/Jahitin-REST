1c9ee85a1280c3c52ebd7b578519bffa3a965bf7
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2023-11-14 06:07:40 +0700

    feat: get idUser

--- src/controller/login.js
+++ src/controller/login.js
@@ -4,7 +4,7 @@ const loginController = async (req, res) => {
     const {username, password} = req.body;
 
     try {
-        const token = await loginModel.login({username, password});
+        const {token, idUser} = await loginModel.login({username, password});
         if (!token) {
             return res.status(401).json({
                 message: 'Invalid credentials'
@@ -12,8 +12,11 @@ const loginController = async (req, res) => {
         }
         res.status(200).json({
             message: 'Login success',
-            token
+            token,
+            idUser
         });
+        console.log("ini token contr", token);
+        console.log("ini idUser contr", idUser);
     } catch (error) {
         console.error('Error during user login:', error);
         res.status(500).json({
