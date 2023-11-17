dabafbb1cbec45e4455e88f79d11642caa402839
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2023-11-17 08:57:38 +0700

    feat : can acept

--- src/controller/subscription.js
+++ src/controller/subscription.js
@@ -25,12 +25,10 @@ const getAllSubscriptionController = async (req, res) => {
         const { body, statusCode } = response;
         console.log(body);
         console.log(statusCode);
-        // response = <?xml version='1.0' encoding='UTF-8'?><S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/"><S:Body><ns2:getAllPendingRequestResponse xmlns:ns2="http://service.jahitin.com/"><return><user_id>1</user_id><status>PENDING</status></return><return><user_id>4</user_id><status>PENDING</status></return></ns2:getAllPendingRequestResponse></S:Body></S:Envelope>
         parseString.parseString(body, (err, result) => {
             if (err) {
                 throw err;
             }
-            // console.log(result);
             console.log(result['S:Envelope']['S:Body'][0]['ns2:getAllPendingRequestResponse'][0]['return']);
             res.status(200).json(result['S:Envelope']['S:Body'][0]['ns2:getAllPendingRequestResponse'][0]['return']);
         });
@@ -39,6 +37,51 @@ const getAllSubscriptionController = async (req, res) => {
     }
 };
 
+const acceptSubscriptionController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const idInt = parseInt(id);
+        console.log(id);
+        console.log(idInt);
+        const status = `ACCEPTED`;
+        console.log(status);
+        console.log('Accept Subscription Controller');
+        const url = 'http://localhost:8003/ws/subscription?wsdl';
+        const headers = {
+            'Content-Type': 'text/xml;charset=UTF-8',
+            'x-api-key': 'RestClient'
+        };
+        const xmlaccsubs =
+        `
+            <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                                xmlns:ser="http://service.jahitin.com/">
+                <soapenv:Header/>
+                <soapenv:Body>
+                    <ser:updateStatus>
+                        <arg0>${status}</arg0>
+                        <arg1>${idInt}</arg1>
+                    </ser:updateStatus>
+                </soapenv:Body>
+            </soapenv:Envelope>
+        `
+        console.log(xmlaccsubs);
+        const { response } = await soapRequest({ url: url, headers: headers, xml: xmlaccsubs});
+        const { body, statusCode } = response;
+        console.log(body);
+        console.log(statusCode);
+        res.status(200).json({message: 'Subscription accepted'});
+        parseString.parseString(body, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+        });
+    } catch(err) {
+        res.status(400).json({message: 'Error while accepting subscription: ' + err.message});
+    }
+}
+
 module.exports = {
-    getAllSubscriptionController
+    getAllSubscriptionController,
+    acceptSubscriptionController
 };
--- src/routes/subscription.js
+++ src/routes/subscription.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 // GET ALL - GET
 router.get('/get', subscriptionController.getAllSubscriptionController);
+router.patch('/acc/:id', subscriptionController.acceptSubscriptionController);
 
 module.exports = router;
 
